b7616941422acd02ca5146b0e12d6932bd931637
Ezequiel Imaz <tavelliezequiel@gmail.com>
2023-12-08 14:46:30 -0300

    Fix previous declared helpers functions

--- src/helpers/components.php
+++ src/helpers/components.php
@@ -1,6 +1,8 @@
 <?php
 
-function _CKEditorPageItem()
-{
-    return \Anonimatrix\PageEditor\Components\CKEditorPageItem::form(...func_get_args());
+if (!function_exists('_CKEditorPageItem')) {
+    function _CKEditorPageItem()
+    {
+        return \Anonimatrix\PageEditor\Components\CKEditorPageItem::form(...func_get_args());
+    }
 }
--- src/helpers/utils.php
+++ src/helpers/utils.php
@@ -1,32 +1,38 @@
 <?php
 
-function getMentionHtml($type)
-{
-	return '<span class="mention" data-mention="'.$type.'">';
+if (!function_exists('getMentionHtml')) {
+	function getMentionHtml($type)
+	{
+		return '<span class="mention" data-mention="'.$type.'">';
+	}
 }
 
-function replaceMention($subject, $type, $replaceWith)
-{
-	$start = strpos($subject, getMentionHtml($type));
-
-	while ($start > -1) {
-
-		$end = strpos($subject, '</span>', $start + strlen(getMentionHtml($type)));
-
-		$subject = substr_replace($subject, $replaceWith, $start, $end - $start);
-
+if (!function_exists('replaceMention')) {
+	function replaceMention($subject, $type, $replaceWith)
+	{
 		$start = strpos($subject, getMentionHtml($type));
 
-	}
+		while ($start > -1) {
 
-	return $subject;
+			$end = strpos($subject, '</span>', $start + strlen(getMentionHtml($type)));
+
+			$subject = substr_replace($subject, $replaceWith, $start, $end - $start);
+
+			$start = strpos($subject, getMentionHtml($type));
+
+		}
+
+		return $subject;
+	}
 }
 
-function replaceAllMentions($text, $mentions = [])
-{
-    collect($mentions)->each(function($mention, $type) use (&$text){
-        $text = replaceMention($text, $type, $mention);
-    });
+if (!function_exists('replaceAllMentions')) {
+	function replaceAllMentions($text, $mentions = [])
+	{
+		collect($mentions)->each(function($mention, $type) use (&$text){
+			$text = replaceMention($text, $type, $mention);
+		});
 
-    return $text;
+		return $text;
+	}
 }
