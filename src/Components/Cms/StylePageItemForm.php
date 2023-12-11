30646f0cf12fb3c43b94c91601f3ccd635f572d4
Ezequiel Imaz <tavelliezequiel@gmail.com>
2023-12-11 11:13:39 -0300

    Fixes in styles names

--- src/Components/Cms/StylePageItemForm.php
+++ src/Components/Cms/StylePageItemForm.php
@@ -18,23 +18,23 @@ class StylePageItemForm extends Form
         $style = $this->model->content;
 
         return _Rows(
-            _InputNumber('campaign.font-size')->name('font_size', false)->value($style->font_size)->class('mb-2'),
-            _Input('campaign.background-color')->type('color')->value($style->background_color)->name('background_color', false)->class('mb-2'),
+            _InputNumber('campaign.font-size')->name('font-size', false)->value($style->font_size)->class('mb-2'),
+            _Input('campaign.background-color')->type('color')->value($style->background_color)->name('background-color', false)->class('mb-2'),
             _Columns(
-                _Input('campaign.text-color')->type('color')->value($style->color)->name('text_color', false)->class('mb-2'),
+                _Input('campaign.text-color')->type('color')->value($style->color)->name('color', false)->class('mb-2'),
             )->class('!mb-0'),
             _Card(
                 _Html('campaign.custom-padding-and-styles')->class('text-sm font-semibold mb-4'),
                 _Html('campaign.padding-px')->class('font-semibold text-sm mb-1'),
                 _Columns(
-                    _Input()->placeholder('campaign.top')->name('padding_top', false)->value($style->padding_top)->class('whiteField'),
-                    _Input()->placeholder('campaign.right')->name('padding_right', false)->value($style->padding_top)->class('whiteField'),
-                    _Input()->placeholder('campaign.bottom')->name('padding_bottom', false)->value($style->padding_top)->class('whiteField'),
-                    _Input()->placeholder('campaign.left')->name('padding_left', false)->value($style->padding_top)->class('whiteField'),
+                    _Input()->placeholder('campaign.top')->name('padding-top', false)->value($style->padding_top)->class('whiteField'),
+                    _Input()->placeholder('campaign.right')->name('padding-right', false)->value($style->padding_top)->class('whiteField'),
+                    _Input()->placeholder('campaign.bottom')->name('padding-bottom', false)->value($style->padding_top)->class('whiteField'),
+                    _Input()->placeholder('campaign.left')->name('padding-left', false)->value($style->padding_top)->class('whiteField'),
                 ),
                 _Input()->placeholder('campaign.styles')
                     ->name('styles', false)
-                    ->value((string) $this->model->content)
+                    ->value((string) $style)
                     ->class('whiteField'),
                 _Input()->placeholder('campaign.classes')->name('classes')->class('whiteField'),
             )->class('bg-gray-100 p-4'),
