5654ace8853b14dd109822af45c725cd936453a3
Ezequiel <tavelliezequiel@gmail.com>
2024-02-15 11:18:09 -0300

    Fix back to articles in editor

--- src/Components/Wiki/Forms/ArticlePageContentForm.php
+++ src/Components/Wiki/Forms/ArticlePageContentForm.php
@@ -14,7 +14,7 @@ class ArticlePageContentForm extends PageContentForm
     protected function top()
     {
         return _FlexBetween(
-            _Link('cms::wiki.back-to-articles')->icon('arrow-left')->knowledgeDrawer(ArticleRawList::class)->class('mb-4'),
+            _Link('cms::wiki.back-to-articles')->icon('arrow-left')->href('knowledge-list')->class('mb-4'),
             !$this->model->id ? null : _Link('cms::wiki.article-in-list')->knowledgeDrawer(ArticlePage::class, ['id' => $this->model->id])->class('mb-4'),
         );
     }
